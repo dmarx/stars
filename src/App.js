1166456f83113c59fdca9e2c6d1c653feeceead5
David Marx <david.marx84@gmail.com>
2024-10-06 22:07:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        {/* Add other routes as needed */}
-      </Switch>
-    </Router>
+    <div className="App">
+      <Dashboard />
+    </div>
   );
 }
 
