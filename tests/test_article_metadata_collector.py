f54ba913caa5dcb7c8db42231354c265f622f7ec
David Marx <david.marx84@gmail.com>
2024-10-09 00:39:57 -0700

    Update test_article_metadata_collector.py

--- tests/test_article_metadata_collector.py
+++ tests/test_article_metadata_collector.py
@@ -7,7 +7,18 @@ import os
 # Add the parent directory to the Python path to import the main script
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from arxiv_metadata_collector import extract_arxiv_id, parse_bibtex, extract_identifier, deduplicate_papers, process_papers, fetch_semantic_scholar_data_batch
+from arxiv_metadata_collector import (
+    extract_arxiv_id, 
+    parse_bibtex,
+    extract_identifier,
+    deduplicate_papers,
+    process_papers,
+    fetch_arxiv_metadata
+    fetch_semantic_scholar_data
+    fetch_semantic_scholar_data_batch,
+    load_existing_data,
+    save_data,
+)
 from utils import controlled_request
 
 def test_extract_identifier():
